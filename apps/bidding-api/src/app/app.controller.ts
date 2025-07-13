f5b331042cfa4cb86b62d64fe7539d20f6e24d0a
thetinshusasi <thetinshusasi@gmail.com>
2025-07-13 09:51:36 -0300

    feat(app): Ticket 3 - Pino Intergration Add Pino Integration
    Added Pino logger integration via middlewares

--- apps/bidding-api/src/app/app.controller.ts
+++ apps/bidding-api/src/app/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Logger } from 'nestjs-pino';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly logger: Logger
+  ) {}
 
   @Get()
   getData() {
+    this.logger.log('Getting data');
     return this.appService.getData();
   }
 }
--- apps/bidding-api/src/app/app.module.ts
+++ apps/bidding-api/src/app/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { LoggerMiddleware } from '../common/middleware/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty' }
+            : undefined,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
